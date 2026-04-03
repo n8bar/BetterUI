33e855e890b7ab1c3c47a89b7977a77004e2b03b
Nate Barlow <nate@nateTheProgrammer.com>
2026-04-02 21:00:00 -0600

    Raise enemy stars much closer to the bar

--- BetterUI/GameClasses/BetterEnemyHud.cs
+++ BetterUI/GameClasses/BetterEnemyHud.cs
@@ -23,7 +23,7 @@ public static class BetterEnemyHud
 
 	private const string PlayerHpPrefix = "BU_playerHPText";
 
-	private const float EnemyStarsLift = 5f;
+	private const float EnemyStarsLift = 45f;
 
 	public static readonly float maxDrawDistance = 3f;
 
